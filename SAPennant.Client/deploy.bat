d8571c33cb39da248e023dde398d03917300e37f
Michael Cherry <zharradam@hotmail.com>
2026-04-11 03:12:44 +0930

    Added the sitemap

--- SAPennant.Client/deploy.bat
+++ SAPennant.Client/deploy.bat
@@ -5,6 +5,7 @@ echo Build step complete.
 if exist "dist\SAPennant\browser\index.html" (
   echo sapennantgolf.com> dist\SAPennant\browser\CNAME
   copy "..\google4134bd2769fffc0b.html" "dist\SAPennant\browser\google4134bd2769fffc0b.html"
+  copy "..\sitemap.xml" "dist\SAPennant\browser\sitemap.xml"
   echo Deploying to GitHub Pages...
   cmd /c npx angular-cli-ghpages --dir=dist/SAPennant/browser
   echo Done.
