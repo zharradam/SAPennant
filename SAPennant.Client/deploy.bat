a1d91d5ee6d1cf52b4b419913eb00e243858621b
Michael Cherry <zharradam@hotmail.com>
2026-04-11 03:09:13 +0930

    Added google html to root

--- SAPennant.Client/deploy.bat
+++ SAPennant.Client/deploy.bat
@@ -4,6 +4,7 @@ cmd /c ng build --configuration production --base-href "https://sapennantgolf.co
 echo Build step complete.
 if exist "dist\SAPennant\browser\index.html" (
   echo sapennantgolf.com> dist\SAPennant\browser\CNAME
+  copy "..\google4134bd2769fffc0b.html" "dist\SAPennant\browser\google4134bd2769fffc0b.html"
   echo Deploying to GitHub Pages...
   cmd /c npx angular-cli-ghpages --dir=dist/SAPennant/browser
   echo Done.
